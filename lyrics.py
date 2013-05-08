d89cee75d11a611efcfe778b8f0225833a495846
Student <student@ent-v321-012.local>
2013-05-08 19:35:49 -0400

    commit 1

---
+++ lyrics.py
@@ -0,0 +1,41 @@
+"""
+lyrics.py
+=====
+* write three programs that all do the same thing!
+* all of these programs should:
+  * print out the same four lines from your favorite song
+  * each line of lyrics should be printed out on a new line
+  * for example, if your program prints out the song, 'On a Tropical Island', 
+    the output would look like (of course, with three version, imagine this
+    three times): 
+
+On a tropical island,
+Underneath a molten lava moon.
+Hangin' with the hula dancers,
+Askin' questions cause' they got all the answers.
+
+* there are multiple ways of printing out strings on new lines:
+  * maybe it's just one command
+  * or several commands
+  * or use different ways of representing strings, putting strings 
+    together, or incorporating newlines
+* write THREE DIFFERENT versions of this program in this file 
+"""
+print("""You don't need no gypsy to tell you why
+You can't let one precious day slip by.
+Look inside yourself, and if you don't see what you want,
+Maybe sometimes then you don't,
+But leave your mind alone and just get high.""")
+
+print("Tear the roof off,")
+print("we're gonna tear the roof off the mother, sucker")
+print("Tear the roof off the sucker")
+
+print("""You've got a real type of thing going down,\n gettin' down
+There's a whole lot of rhythm going round\n""")
+
+print("""Ow, we want the funk\n
+Give up the funk\n
+Ow, we need the funk\n
+We gotta have that funk""")
+
